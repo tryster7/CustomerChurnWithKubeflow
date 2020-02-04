d5b4cf81e8757d683f8f003ae033b6fca82f1e81
tryster7 <59949804+tryster7@users.noreply.github.com>
2020-02-04 12:06:54 +0530

    fixed param name

--- client/client.py
+++ client/client.py
@@ -10,8 +10,8 @@ def get_prediction(server_host='127.0.0.1', server_port=8500, model_name='ccd'):
     testX = testX.drop(testX.columns[0], axis=1)
     print(testX.head())
     testy = pd.read_csv("gs://kube-1122/customerchurn/output/test_label.csv")
-    testX = testy.drop(testy.columns[0], axis=1)
-    print("predictions ...\n" , testy.head())
+    testy = testy.drop(testy.columns[0], axis=1)
+    print("test labels ...\n" , testy.head())
     data = json.dumps({"signature_name": "serving_default", "instances": testX[0:5].values.tolist()})
     headers = {"content-type": "application/json"}
     json_response = requests.post('http://' + server_host + ':' + str(server_port) + '/v1/models/' + str(model_name) + ':predict',
@@ -27,4 +27,4 @@ if __name__ == '__main__':
     server_host = sys.argv[1]
     server_port = int(sys.argv[2])
     model_name = sys.argv[3]
-    get_prediction(server_host=server_host, server_port=server_port)
+    get_prediction(server_host=server_host, server_port=server_port,model_name=model_name)
