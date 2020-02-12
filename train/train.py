8fdd9d93d0b81ee59edd93c0c67241aa7d49f400
tryster7 <59949804+tryster7@users.noreply.github.com>
2020-02-12 11:58:36 +0530

    change optimizer from SGD to Adam

--- train/train.py
+++ train/train.py
@@ -83,8 +83,8 @@ def train(bucket_name, epochs, batch_size, katib, optimizer_name):
     
     dnn = create_tfmodel(
         # optimizer=tf.keras.optimizers.get(optimizer_name),
-        optimizer=tf.keras.optimizers.get('SGD'),     
-        # optimizer=tf.keras.optimizers.get('Adam'),
+        # optimizer=tf.keras.optimizers.get('SGD'),     
+        optimizer=tf.keras.optimizers.get('Adam'),
         loss='binary_crossentropy',
         metrics=['accuracy'],
         input_dim=trainX.shape[1])
